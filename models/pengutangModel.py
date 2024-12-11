2701c3eadbd1aa1737d2356f9a62caa55d1d6a33
karinafs-yo <karinafs@upi.edu>
2024-12-11 13:35:41 +0700

    Menambahkan generate random id

--- models/pengutangModel.py
+++ models/pengutangModel.py
@@ -12,3 +12,8 @@ def savePengutang(pengutang):
     with open(pengutangFile, "w") as f:
         json.dump(pengutang, f, indent=4)
 
+def generateRandomID(existing_ids, length=4):
+    while True:
+        new_id = ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
+        if new_id not in existing_ids:  # Pastikan ID unik
+            return new_id
