de6b2b75ed66b2b774ae042607d15735030abb58
Bintang Kurniawan <bintang.kurniawannn.98@gmail.com>
2024-12-11 14:55:29 +0700

    little fix

--- views/loginView.py
+++ views/loginView.py
@@ -1,7 +1,18 @@
 from controllers.login.login import login_controller
 def login():
-    username = input("Masukkan username: ")
-    password = input("Masukkan password: ")
+    attempts = 3
+    while attempts > 0:
+        username = input("Masukkan username: ")
+        password = input("Masukkan password: ")
 
-    login_controller(username, password)
+        if login_controller(username, password):
+            print("Login successful! Welcome!")
+            return
+        else:
+            attempts -= 1
+            print(f"Password atau username salah. Anda memiliki {attempts} kesempatan lagi.")
+
+        if attempts == 0:
+            print("Anda telah gagal login 3 kali. Silakan coba lagi nanti.")
+            exit()
 
