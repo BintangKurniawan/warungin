2cd1f2fee9e79222536f67ebb698e65f527500cb
karinafs-yo <karinafs@upi.edu>
2024-12-11 13:34:09 +0700

    Menambahkan save pengutang

--- models/pengutangModel.py
+++ models/pengutangModel.py
@@ -8,3 +8,7 @@ def loadDataPengutang():
     with open(pengutangFile, "r") as f:
         return json.load(f)
     
+def savePengutang(pengutang):
+    with open(pengutangFile, "w") as f:
+        json.dump(pengutang, f, indent=4)
+
