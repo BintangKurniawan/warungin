16d90ba3fe4f8fd9a4212b1c95c5035d46e6061d
sitimasda15 <mazdasiti15@gmail.com@domain.com>
2024-12-18 13:24:45 +0700

    menambahkan dungsi validasi di addProduct

--- controllers/products/addProduct.py
+++ controllers/products/addProduct.py
@@ -2,10 +2,31 @@ from models.productsModel import loadProducts, generateRandomID, saveProducts
 
 def tambahProduk():
     dataProduct = loadProducts()
+    while True:
+        namaProduk = input("Masukkan nama produk: ").title()
 
-    namaProduk = input("Masukkan nama produk: ").title()
-    stokProduk = int(input("Masukkan stok produk: "))
-    hargaProduk = int(input("Masukkan harga produk: "))
+        if not namaProduk.strip():
+            print("Nama produk tidak boleh kosong, silahkan masukkan nama produk.")
+        else:
+            break
+
+    while True:
+        try:
+            stokProduk = int(input("Masukkan stok produk: "))
+            if stokProduk < 0 :
+                print("Stok produk harus bernilai postif. Silahkan maasukkan lagi stok produk dengan benar")
+            else:
+                break
+        except ValueError: print("Stok produk harus berupa angka.")
+
+    while True:
+        try:
+            hargaProduk = int(input("Masukkan harga produk: "))
+            if hargaProduk < 0:
+                print("Harga produk harus bernilai positif. Silahkan masukkan lagi hargaproduk dengan benar.")
+            else:
+                break
+        except ValueError: print("Harga produk harus berupa angka")
 
     existing_ids = {item["id"] for item in dataProduct}
 
