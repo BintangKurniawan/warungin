083aff7ca85c324a4435a1e12a7962633aa8ea6d
karinafs-yo <karinafs@upi.edu>
2024-12-18 13:49:17 +0700

    Tambah keterangan di delete produk

--- controllers/products/deleteProduct.py
+++ controllers/products/deleteProduct.py
@@ -2,18 +2,26 @@ from models.productsModel import loadProducts, saveProducts
 
 def hapusProduk():        
     data_produk = loadProducts()
+    while True:
+        
+        id_produk = input("Masukkan ID produk: ").upper()
 
-    id_produk = input("Masukkan ID produk: ").upper()
+        original_length = len(data_produk)
 
-    original_length = len(data_produk)
+        # menulis ulang semua data produk kecuali produk yang ingin dihapus
+        data_produk = [product for product in data_produk if product["id"] != id_produk]
 
-    # menulis ulang semua data produk kecuali produk yang ingin dihapus
-    data_produk = [product for product in data_produk if product["id"] != id_produk]
+        if len(data_produk) == original_length:
+            print("Error: ID produk tidak ditemukan.")
+            pilihan = input("apakah anda ingin menghapus produk lain?")
+            if pilihan == "y":
+                continue
+            else:
+                print("Penghapusan dibatalkan")
+                return
+        else:
 
-    if len(data_produk) == original_length:
-        print("Error: ID produk tidak ditemukan.")
-        return
+            saveProducts(data_produk)
 
-    saveProducts(data_produk)
-
-    print("Produk berhasil dihapus")
+            print("Produk berhasil dihapus")
+            break
