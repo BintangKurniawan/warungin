bb0011baec7931c58666999499e27e4461751eec
Bintang Kurniawan <bintang.kurniawannn.98@gmail.com>
2024-12-18 13:53:35 +0700

    validate add pengutang

--- controllers/pengutang/editPengutang.py
+++ controllers/pengutang/editPengutang.py
@@ -2,7 +2,8 @@ from InquirerPy import inquirer
 from models.pengutangModel import loadDataPengutang, savePengutang
 from datetime import datetime
 from tabulate import tabulate
-
+import re
+import os
 def editPengutang():
     dataPengutang = loadDataPengutang()
 
@@ -27,13 +28,34 @@ def editPengutang():
             ).execute()
 
             if answer == "Nama pengutang":
-                pengutangTerpilih["nama pengutang"] = input("Masukkan nama pengutang baru: ").title()
-                print("Nama pengutang berhasil diedit")
+                while True:
+                    pengutangTerpilih["nama pengutang"] = input("Masukkan nama pengutang baru: ").title()
+
+                    if not pengutangTerpilih["nama pengutang"].strip():
+                        print("Nama pengutang tidak boleh kosong. Silakan masukkan kembali nama pengutang.")
+                    else:
+                        print("Nama pengutang berhasil diedit")
+                        break
             elif answer == "Total hutang":
-                pengutangTerpilih["total hutang"] = input("Masukkan total hutang baru: ")
-                print("Total hutang berhasil diedit")
+                while True:
+                    try:
+                        pengutangTerpilih["total hutang"] = int(input("Masukkan total hutang baru: "))
+                        if pengutangTerpilih["total hutang"] < 0:
+                            print("Total hutang tidak boleh kurang dari 0 rupiah. Silakan masukkan kembali total hutang.")
+                        elif pengutangTerpilih["total hutang"] > 50000:
+                            print("Total hutang melebihi batas maksimal 50000 rupiah. Silakan masukkan kembali total hutang.")
+                        else:
+                            break
+                    except ValueError:
+                        print("Total hutang harus berupa angka. Silakan masukkan kembali total hutang.")
+
+                
             elif answer == "Barang":
                 while True:
+                    os.system('cls')
+                    headers = ["No", "Nama Produk", "Jumlah Produk", "Tanggal Hutang"]
+                    table = [[i + 1, barang["nama produk"], barang["jumlah produk"], barang["tanggal hutang"]] for i, barang in enumerate(pengutangTerpilih["barang"])]
+                    print(tabulate(table, headers=headers, tablefmt="grid", stralign="center", numalign="center"))
                     barangAnswer = inquirer.select(
                         message="Pilih aksi untuk barang:",
                         choices=["Tambah barang", "Edit barang", "Hapus barang", "Kembali"],
@@ -41,8 +63,25 @@ def editPengutang():
                     ).execute()
 
                     if barangAnswer == "Tambah barang":
-                        namaProduk = input("Masukkan nama produk: ").title()
-                        jumlahProduk = int(input("Masukkan jumlah produk: "))
+
+                        while True:
+                            namaProduk = input("Masukkan nama produk: ").title()
+
+                            if not namaProduk.strip():
+                                print("Nama produk tidak boleh kosong. Silakan masukkan kembali nama produk.")
+                            else:
+                                break
+
+                        while True:
+                            try:
+                                jumlahProduk = int(input("Masukkan jumlah produk: "))
+                                if jumlahProduk < 0:
+                                    print("Jumlah produk tidak boleh kurang dari 0. Silakan masukkan kembali jumlah produk.")
+                                else:
+                                    break
+                            except ValueError:
+                                print("Jumlah produk harus berupa angka. Silakan masukkan kembali jumlah produk.")
+
                         tanggalHutang = datetime.now().strftime("%Y-%m-%d")
                         
                         barangBaru = {
@@ -58,18 +97,56 @@ def editPengutang():
                             print("Tidak ada barang untuk diedit.")
                             continue
 
-                        headers = ["No", "Nama Produk", "Jumlah Produk", "Tanggal Hutang"]
-                        table = [[i + 1, barang["nama produk"], barang["jumlah produk"], barang["tanggal hutang"]] for i, barang in enumerate(pengutangTerpilih["barang"])]
-                        print(tabulate(table, headers=headers, tablefmt="grid", stralign="center", numalign="center"))
-
                         indexBarang = int(input("Pilih nomor barang yang akan diedit: ")) - 1
                         if 0 <= indexBarang < len(pengutangTerpilih["barang"]):
                             barangTerpilih = pengutangTerpilih["barang"][indexBarang]
-                            
-                            barangTerpilih["nama produk"] = input("Masukkan nama produk baru: ").title()
-                            barangTerpilih["jumlah produk"] = int(input("Masukkan jumlah produk baru: "))
-                            barangTerpilih["tanggal hutang"] = input("Masukkan tanggal hutang baru (YYYY-MM-DD): ")
-                            print("Barang berhasil diedit.")
+                        
+                            while True:
+                                os.system('cls')
+                                headers = ["No", "Nama Produk", "Jumlah Produk", "Tanggal Hutang"]
+                                table = [[i + 1, barang["nama produk"], barang["jumlah produk"], barang["tanggal hutang"]] for i, barang in enumerate(pengutangTerpilih["barang"])]
+                                print(tabulate(table, headers=headers, tablefmt="grid", stralign="center", numalign="center"))
+                                answer = inquirer.select(
+                                    message="Pilih salah satu opsi:",
+                                    choices=["Nama produk", "Jumlah produk", "Tanggal hutang", "Kembali"],
+                                    default="Nama produk"
+                                ).execute()
+
+                                if answer == "Nama produk":
+
+                                    while True:
+                                        barangTerpilih["nama produk"] = input("Masukkan nama produk baru: ").title()
+
+                                        if not barangTerpilih["nama produk"].strip():
+                                            print("Nama produk tidak boleh kosong. Silakan masukkan kembali nama produk.")
+                                        else:
+                                            print("Barang berhasil diedit.")
+                                            break
+                                
+                                elif answer == "Jumlah produk":
+                                    while True:
+                                        try:
+                                            barangTerpilih["jumlah produk"] = int(input("Masukkan jumlah produk baru: "))
+                                            if barangTerpilih["jumlah produk"] < 0:
+                                                print("Jumlah produk tidak boleh kurang dari 0. Silakan masukkan kembali jumlah produk.")
+                                            else:
+                                                print("Barang berhasil diedit.")
+                                                break
+                                        except ValueError:
+                                            print("Jumlah produk harus berupa angka. Silakan masukkan kembali jumlah produk.")  
+
+                                elif answer == "Tanggal hutang":        
+                                    while True:
+                                        barangTerpilih["tanggal hutang"] = input("Masukkan tanggal hutang baru (YYYY-MM-DD): ").strip()
+
+                                        if not re.match(r'^\d{4}-\d{2}-\d{2}$', barangTerpilih["tanggal hutang"]):
+                                            print("Format tanggal tidak valid. Harap masukkan dalam format YYYY-MM-DD dan tanpa huruf.")
+                                        else:
+                                            print("Barang berhasil diedit.")
+                                            break
+                                
+                                elif answer == "Kembali":
+                                    break
                         else:
                             print("Nomor barang tidak valid.")
 
@@ -78,9 +155,6 @@ def editPengutang():
                             print("Tidak ada barang untuk dihapus.")
                             continue
 
-                        headers = ["No", "Nama Produk", "Jumlah Produk", "Tanggal Hutang"]
-                        table = [[i + 1, barang["nama produk"], barang["jumlah produk"], barang["tanggal hutang"]] for i, barang in enumerate(pengutangTerpilih["barang"])]
-                        print(tabulate(table, headers=headers, tablefmt="grid", stralign="center", numalign="center"))
 
                         indexBarang = int(input("Pilih nomor barang yang akan dihapus: ")) - 1
                         if 0 <= indexBarang < len(pengutangTerpilih["barang"]):
